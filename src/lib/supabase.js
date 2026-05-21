ffbf64fafd4c6e3e713d2630e741c916e71bbbf2
Alejunior26 <109094459+Alejunior26@users.noreply.github.com>
2026-05-21 16:14:51 -0300

    Delete src directory

--- src/lib/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl =
-  process.env.REACT_APP_SUPABASE_URL || "https://coydyfllhpmiobhnjgsg.supabase.co";
-const supabaseKey =
-  process.env.REACT_APP_SUPABASE_ANON_KEY || "sb_publishable_v0BGyR8opvlCDD2oGRbQfw_xCoM9JB5";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
